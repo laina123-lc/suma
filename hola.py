3b6842fec66123c26c63a71cf93951ed5ac2d189
laina123-lc <63610953+laina123-lc@users.noreply.github.com>
2020-04-13 14:54:55 -0500

    Update hola.py

--- hola.py
+++ hola.py
@@ -1,9 +1,19 @@
 import os
 import sys
 import math 
-
+ if opcion==1(
 n1=float(input("Intro número uno: "))
 n2=float(input("Intro numero dos: "))
 suma=n1+n2
 
 print("La suma es: ",suma)
+ if opcion==2
+   n1=float(input("Intro primer numero:"))
+   n2=float(input("Intro segundo numero:"))
+   resta=n1-n2
+   
+ print("la resta es:" ,resta)
+   
+  else:
+        print "No es opcion Valida" 
+   
